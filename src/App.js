a95749740b6af0231212bc55b9256353a0363177
argon1025 <argon1025@gmail.com>
2021-01-26 16:25:40 +0900

    Add components/Login.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react';
+import { Header } from './components';
+import { Login } from './components';
+class App extends Component {
+    render() {
+        return (
+            <div>
+                
+                <Login/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from './Header/Header';
+import Login from './Login/Login';
+export {
+    Header,
+    Login
+};
